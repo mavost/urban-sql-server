dac9b96962565978045730ceb081db15cb14b055
Markus von Steht <markus.von.steht@gmx.de>
2025-05-16 12:10:47 +0200

    fixed restoration locations

--- compose/sqlserver_historic/restore-databases.sh
+++ compose/sqlserver_historic/restore-databases.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
+set -e
 
-echo "Restoring WideWorldImporters..."
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -Q "RESTORE DATABASE [WideWorldImporters] FROM DISK = '/var/opt/mssql/backup/WideWorldImporters-Full.bak' WITH MOVE 'WWI_Primary' TO '/var/opt/mssql/data/WideWorldImporters.mdf', MOVE 'WWI_UserData' TO '/var/opt/mssql/data/WideWorldImporters_userdata.ndf', MOVE 'WWI_Log' TO '/var/opt/mssql/data/WideWorldImporters.ldf'"
+echo "$(date) Restoring WideWorldImporters..."
 
-echo "Restoring WideWorldImportersDW..."
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -Q "RESTORE DATABASE [WideWorldImportersDW] FROM DISK = '/var/opt/mssql/backup/WideWorldImportersDW-Full.bak' WITH MOVE 'WWIDW_Primary' TO '/var/opt/mssql/data/WideWorldImportersDW.mdf', MOVE 'WWIDW_Log' TO '/var/opt/mssql/data/WideWorldImportersDW.ldf'"
+# Restore WideWorldImporters
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -Q "
+RESTORE DATABASE WideWorldImporters
+FROM DISK = '/var/opt/mssql/backup/WideWorldImporters-Full.bak'
+WITH MOVE 'WWI_Primary' TO '/var/opt/mssql/data/WideWorldImporters.mdf',
+     MOVE 'WWI_UserData' TO '/var/opt/mssql/data/WideWorldImporters_UserData.ndf',
+     MOVE 'WWI_Log' TO '/var/opt/mssql/data/WideWorldImporters.ldf',
+     MOVE 'WWI_InMemory_Data_1' TO '/var/opt/mssql/data/WideWorldImporters_InMemory_Data_1',
+     REPLACE
+"
 
-echo "Restore complete."
+echo "$(date) Restoring WideWorldImportersDW..."
+
+# Restore WideWorldImportersDW
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -Q "
+RESTORE DATABASE WideWorldImportersDW
+FROM DISK = '/var/opt/mssql/backup/WideWorldImportersDW-Full.bak'
+WITH MOVE 'WWI_Primary' TO '/var/opt/mssql/data/WideWorldImportersDW.mdf',
+     MOVE 'WWI_UserData' TO '/var/opt/mssql/data/WideWorldImportersDW_UserData.ndf',
+     MOVE 'WWI_Log' TO '/var/opt/mssql/data/WideWorldImportersDW.ldf',
+     MOVE 'WWIDW_InMemory_Data_1' TO '/var/opt/mssql/data/WideWorldImportersDW_InMemory_Data_1',
+     REPLACE
+"
+
+echo "$(date) Restore complete."
